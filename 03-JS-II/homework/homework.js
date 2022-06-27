90e0ea877fb47a806e84b5a8106aba5ecce1ff2f
Fpilafis <fpilafis@hotmail.com>
2022-06-27 11:21:32 -0300

    Resuelta la tarea esEntero(numero)

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -108,6 +108,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  return Math.floor(numero)==numero
 }
 
 function fizzBuzz(numero) {
