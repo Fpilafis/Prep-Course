a95acc3371e2f77d25c441ecac51d8827b6df5e2
Fpilafis <fpilafis@hotmail.com>
2022-06-27 14:51:29 -0300

    Homework JSIII. incrementarPorUno(array)

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for(let i=0; i<array.length; i++) {
+    array[i] = array [i] + 1
+  }
+  return array
 }
 
 
