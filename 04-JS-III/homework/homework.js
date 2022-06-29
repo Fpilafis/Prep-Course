6af95fed7d9a37801e4aa3b3f32da9658f1d15e2
Fpilafis <fpilafis@hotmail.com>
2022-06-29 12:08:39 -0300

    Homework JSIII. Tarea todosIguales(arreglo) terminada

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -186,7 +186,12 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  
+  for(let i=0; i<arreglo.length; i++){
+    if(arreglo[i]!== arreglo[++i]) {
+      return false
+    }
+  }
+  return true
 } 
 
 
