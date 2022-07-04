9bb33b43bbe3558ef22a5ebcee3c898c0c45f6a4
Fpilafis <fpilafis@hotmail.com>
2022-07-04 11:15:48 -0300

    Homework JSIV. Tarea agregarPropiedad (objeto,property) terminada

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -21,8 +21,8 @@ function agregarPropiedad (objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
- obj[property] = null
- return obj
+  objeto[property] = null;
+  return objeto
 }
 
 function invocarMetodo (objeto, metodo) {
