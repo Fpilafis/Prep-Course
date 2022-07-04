fafaf8af9c016efb4921a244f04efdbd3795fe0c
Fpilafis <fpilafis@hotmail.com>
2022-07-04 11:54:42 -0300

    Homework JSIV. Tarea nuevoUsuario(nombre, email, password) y tieneEmail(usuario) terminada

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -54,13 +54,20 @@ function nuevoUsuario (nombre, email, password) {
   // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
   // Devuelve el objeto
   // Tu código:
-
+  var objet = {
+    nombre: nombre,
+    email: email,
+    password: password,
+  }
+  return objet
 }
 
 function tieneEmail (usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
+  return usuario.email != null;
+
 }
 
 function tienePropiedad (objeto, propiedad) {
