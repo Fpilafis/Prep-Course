70e4f29ce611f8d447cc05aaf5264d26b3a3ea3d
Fpilafis <fpilafis@hotmail.com>
2022-07-04 11:40:29 -0300

    Homework JSIV. Tarea multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) terminada

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -37,7 +37,8 @@ function multiplicarNumeroDesconocidoPorCinco (objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
-
+  var total = objetoMisterioso.numeroMisterioso*5;
+  return total
 }
 
 function eliminarPropiedad (objeto, unaPropiedad) {
